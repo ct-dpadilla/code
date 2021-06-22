4eddf158bdd38df61f1e3fccf9a7e097c11d50b7
ct-dpadilla <85650008+ct-dpadilla@users.noreply.github.com>
2021-06-22 13:44:07 -0700

    Update install-adobe-pkg.sh

--- install-adobe-pkg.sh
+++ install-adobe-pkg.sh
@@ -2,6 +2,8 @@
 sudo mv /Applications/Jamf-Acrobat-Install/AcroRdrDC_2100520048_MUI.pkg /Applications/
 
 sudo installer -pkg /Applications/Acro*.pkg -target /Applications
-#sudo rm /Applications/Adobe*.pkg
+
+sudo rm /Applications/Adobe*.pkg
+
 sudo rm -r /Applications/Jamf-Acrobat-install
 
