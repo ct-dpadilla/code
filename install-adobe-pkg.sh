2b1f41033d43856fa057488d49677ab130e15037
ct-dpadilla <85650008+ct-dpadilla@users.noreply.github.com>
2021-06-22 13:20:46 -0700

    installing abode acrobat dmg
    updating code:
    have jamf create a directory with the library and private folder 
    runs command to install pkg
    deletes the folder

--- install-adobe-pkg.sh
+++ install-adobe-pkg.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-sudo installer -pkg /Applications/Adobe*.pkg -target /Applications
-sudo rm /Applications/Adobe*.pkg
+sudo installer -pkg /Applications/Jamf-Acrobat-Install/Adobe*.pkg -target /Applications
+#sudo rm /Applications/Adobe*.pkg
+sudo rm -r /Applications/Jamf-Acrobat-install
